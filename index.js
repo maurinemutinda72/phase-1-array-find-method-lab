afbaeac6f30d8195f5803c0e97b8fd7c3d622407
moureen mutinda <maurinemutinda72@gmail.com>
2024-12-19 12:06:04 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function superbowlWin(record) {
+    const win = record.find(game => game.result === "W");
+    return win ? win.year : undefined;
+  }
+  const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+  ];
+  
+  console.log(superbowlWin(record)); // Output: "2015"
+  
+  const noWinRecord = [
+    { year: "2015", result: "L" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+  ];
+  
+  console.log(superbowlWin(noWinRecord)); // Output: undefined
+  
